2ed8541dc7f21e90ab9991495af5d89da18f0261
Ilya Kolosov <iiilyya.kolosov@gmail.com>
2026-09-24 19:08:47 +0300

    feat(lab-1): connect CSV reader and type writer

--- lab-1/src/main/java/ru/kolosov/lab1/Main.java
+++ lab-1/src/main/java/ru/kolosov/lab1/Main.java
@@ -1,8 +1,23 @@
 package ru.kolosov.lab1;
 
+import ru.kolosov.lab1.reader.CharacterTypeReader;
+import ru.kolosov.lab1.writer.TypeFileWriter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import java.util.HashSet;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Java работает!");
+    public static void main(String[] args) throws IOException{
+        Path inputFile = Path.of("lab-1/src/main/resources/characters.csv");
+        Path outputFile = Path.of("lab-1/types.txt");
+
+        CharacterTypeReader reader = new CharacterTypeReader();
+        TypeFileWriter writer = new TypeFileWriter();
+
+        HashSet<String> types = reader.readTypes(inputFile);
+        writer.writeTypes(outputFile, types);
     }
 }
