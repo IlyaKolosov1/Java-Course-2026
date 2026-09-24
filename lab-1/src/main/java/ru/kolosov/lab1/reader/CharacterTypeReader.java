ed1438f6b803a0a7e420e37c1104916fd4573f8d
Ilya Kolosov <iiilyya.kolosov@gmail.com>
2026-09-24 18:44:17 +0300

    feat(lab-1): read unique character types from CSV

---
+++ lab-1/src/main/java/ru/kolosov/lab1/reader/CharacterTypeReader.java
@@ -0,0 +1,42 @@
+package ru.kolosov.lab1.reader;
+
+import java.nio.file.Path; // Тип данных 
+import java.util.HashSet; // Коллекция
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+
+public class CharacterTypeReader {
+
+    // public - can be executed from Main
+    public HashSet<String> readTypes(Path inputFile) throws IOException{
+        HashSet<String> types = new HashSet<>();
+
+
+        // try-with-resources
+        try (BufferedReader reader = Files.newBufferedReader(inputFile, StandardCharsets.UTF_8)){
+
+            reader.readLine();
+
+            String line;
+
+            while((line = reader.readLine()) != null){
+                String[] columns = line.split(",", -1);
+
+                if (columns.length <= 4) {
+                    throw new IllegalArgumentException("Incorrect string CSV " + line);
+                }
+
+                String type = columns[4].trim();
+
+                if (!type.isBlank()) {
+                    types.add(type);
+                }
+            }
+        }
+        return types;
+    }
+}
